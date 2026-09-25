bf022dfd000a4dfb84737b360bdb6af03c33e437
user500050 <gigametr2@gmail.com>
2026-09-25 07:50:26 +0500

    Update to v.1.1.0
    - Better ModMenu

---
+++ FsmDiagnostics.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using HutongGames.PlayMaker;
+
+namespace MidAirHealing
+{
+	internal sealed class FsmDiagnostics
+	{
+		private readonly Action<string> Log;
+
+		public FsmDiagnostics(Action<string> log)
+		{
+			Log = log;
+		}
+
+		public void DumpHealingStates(PlayMakerFSM component)
+		{
+			DumpState(component, "Focus Start");
+			DumpState(component, "Focus");
+			DumpState(component, "Focus S");
+			DumpState(component, "Focus Left");
+			DumpState(component, "Focus Right");
+		}
+
+		private void DumpState(PlayMakerFSM component, string stateName)
+		{
+			var state = component.Fsm.GetState(stateName);
+
+			if (state == null)
+			{
+				Log("State not found: " + stateName);
+				return;
+			}
+
+			Log("=== " + stateName + " ===");
+
+			for (int i = 0; i < state.Actions.Length; i++)
+			{
+				var action = state.Actions[i];
+
+				Log("Action " + i + ": " + action.GetType().FullName
+					+ "; enabled = " + action.Enabled);
+
+				var fields = action.GetType().GetFields(
+	BindingFlags.Public | BindingFlags.Instance);
+
+				foreach (var field in fields)
+				{
+					object value = field.GetValue(action);
+
+					string text;
+
+					if (value == null)
+						text = "<null>";
+					else if (value is FsmEvent fsmEvent)
+						text = fsmEvent.Name;
+					else
+						text = value.ToString();
+
+					Log("    " + field.Name + " = " + text);
+				}
+			}
+
+			foreach (var transition in state.Transitions)
+			{
+				Log("Transition: " + transition.EventName
+					+ " -> " + transition.ToState);
+			}
+		}
+	}
+}
